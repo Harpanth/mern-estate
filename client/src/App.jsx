973f87af67444f0d5c6bd17bcd4c829cb632601b
harpanth <19116@iiitu.ac.in>
2025-04-19 17:56:55 +0530

    "Routes"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
-function App() {
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
+import About from './pages/About'
 
-
+const App = () => {
   return (
-    <h1 className="text-red-500">Hi</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+
+      </Routes >
+    </BrowserRouter>
   )
 }
 
