45802171dbdd45d3142f12858698fa4ec5f411a4
harpanth <19116@iiitu.ac.in>
2025-04-30 14:27:18 +0530

    "Added image Slider"

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { createListing,deleteListing } from '../controller/listing.controller.js';
+import { createListing,deleteListing,updateListing,getListing } from '../controller/listing.controller.js';
+import { verifyToken } from '../utils/veriftToken.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteListing)
+router.post('/update/:id',verifyToken,updateListing)
+router.get('/get/:id', getListing)
 r
