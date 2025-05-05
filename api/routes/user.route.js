b9be375f488375a93d3e462e75abb7eef537a7e9
harpanth <19116@iiitu.ac.in>
2025-05-05 10:44:43 +0530

    "Add contact landlord functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, test, updateUser,getUserListings } from '../controller/user.controller.js'
+import { deleteUser, test,getUser, updateUser,getUserListings } from '../controller/user.controller.js'
 import { verifyToken } from '../utils/veriftToken.js'
 // Create a router instance
 const router = express.Router()
@@ -9,6 +9,7 @@ router.get('/test', test)
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('delete/:id',verifyToken,deleteUser)
 router.get('/listing/:id',verifyToken,getUserListings)
+router.get('/:id',verifyToken,getUser);
 
 
 // Export the router to be used in your main app
